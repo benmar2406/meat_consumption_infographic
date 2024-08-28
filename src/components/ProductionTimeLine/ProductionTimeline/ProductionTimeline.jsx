7080db4ed720e375e4fd14d7016ea0a7903059f2
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2024-08-28 17:24:30 +0200

    Add files via upload

---
+++ src/components/ProductionTimeLine/ProductionTimeline/ProductionTimeline.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import YearBlock from './YearBlock/YearBlock';
+
+// Function to group data by decades, excluding the 2020s
+const groupDataByDecade = (data) => {
+  const groupedData = {};
+
+  data.forEach((item) => {
+    const decade = Math.floor(item.Year / 10) * 10;
+
+    // Exclude the 2020s data
+    if (decade >= 2020) {
+      return;
+    }
+
+    if (!groupedData[decade]) {
+      groupedData[decade] = {
+        decade,
+        totalProduction: 0,
+      };
+    }
+    groupedData[decade].totalProduction += item["Total Meat production (tonnes)"];
+  });
+
+  return Object.values(groupedData);
+};
+
+const ProductionTimeline = ({ data }) => {
+  const groupedData = groupDataByDecade(data);
+
+  return (
+    <>  
+        <section className='production-timeline'>
+            <div className='chart-container-timeline'>
+            <h2>Development of meat production over the decades</h2>
+            {groupedData.map((d) => (
+                <YearBlock key={d.decade} year={`${d.decade}s`} production={d.totalProduction} />
+            ))}
+            </div>
+        </section>
+    </>
+  );
+};
+
+export default ProductionTimeline;
