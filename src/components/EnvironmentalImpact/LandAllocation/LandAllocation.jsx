1318e16b72ef93af4a3d9052a1a03aeb1ee612e8
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2024-09-02 22:31:10 +0200

    land allocation

--- src/components/EnvironmentalImpact/LandAllocation/LandAllocation.jsx
+++ src/components/EnvironmentalImpact/LandAllocation/LandAllocation.jsx
@@ -1,51 +1,62 @@
-import React, { useRef } from 'react';
-import { useScroll, useTransform, motion, useMotionValue, useMotionValueEvent, useInView } from "framer-motion";
+import React, { useRef, useState, useEffect } from 'react';
+import { useScroll, useTransform, motion, useInView } from "framer-motion";
 import './LandAllocation.css';
 
 const LandAllocation = () => {
   
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+  const refScrollContainer = useRef(null);
+  const isInView = useInView(refScrollContainer, { once: true });
 
-  // Using useScroll to track the scroll position of the target element
   const { scrollYProgress } = useScroll({
-    target: ref
+    target: refScrollContainer
   });
 
-  // Convert the scrollYProgress to a width value for the progress bar
-  const width = useTransform(scrollYProgress, [0, 0.8], ["0%", "100%"]);
-  
-  // Convert scrollYProgress to a percentage for display
+  const width = useTransform(scrollYProgress, [0, 0.9], ["0%", "100%"]);
   const percentage = useTransform(scrollYProgress, [0, 1], [0, 100]);
 
-  // Use useMotionValue to get the latest value of the percentage
-  const percentageValue = useMotionValue(0);
+  const [displayChartText, setDisplayChartText] = useState(false);
 
-  // Update the percentage value as the scrollYProgress changes
-  useMotionValueEvent(percentage, "change", (latest) => {
-    percentageValue.set(latest.toFixed(0)); // Round to nearest integer
-  });
+  useEffect(() => {
+    const unsubscribe = percentage.onChange((latestPercentage) => {
+      if (latestPercentage >= 70) { 
+        setDisplayChartText(true);
+      } else {
+        setDisplayChartText(false);
+      }
+    });
+  
+    return () => unsubscribe(); 
+  }, [percentage]);
 
   return (
-    <div className='landuse-scroll-container' ref={ref}>
-        <div className='landuse-chart-container'>
-          <div className='landuse-chart'
-            style={{transform: isInView ? "none" : "translateX(-200px)",
-              opacity: isInView ? 1 : 0,
-              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"}}
-          >
-            <div className='landuse-chart-meat'>
-              <motion.div 
-                className='landuse-chart-fill' 
-                style={{ width }} 
-              />
-            </div>
-            <article className='landuse-text'>
-              {percentageValue.get()}% wird für...
-            </article>
+    <section className='landuse-scroll-container' ref={refScrollContainer}>
+      <div className='landuse-chart-container'>
+      <h2 className='landuse-headline'>Allocation of farmland (European Union)</h2>
+        <div className='landuse-chart'
+          style={{transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"}}
+        >
+          <div className='landuse-chart-meat'>
+            <motion.div 
+              className='landuse-chart-fill' 
+              style={{ width }} 
+            >
+              <motion.span className='landuse-chart-fill-text'
+                style={{ 
+                  opacity: displayChartText ? 1 : 0,
+                  transition: "opacity 0.5s ease-in-out"}}
+              >
+                between 60% and 70% of farmland<br /> utilized for meat production.
+              </motion.span>
+            </motion.div>
           </div>
+          <article className='landuse-text'>
+            {/* Additional text or content can be placed here */}
+          </article>
         </div>
       </div>
+    </section>
   );
 }
 
