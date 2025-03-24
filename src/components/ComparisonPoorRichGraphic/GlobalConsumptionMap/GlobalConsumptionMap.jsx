6ab16fcaa2eb833bdda5441d5bdbbdcc836efba4
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-03-24 07:39:43 +0100

    Update GlobalConsumptionMap.jsx

--- src/components/ComparisonPoorRichGraphic/GlobalConsumptionMap/GlobalConsumptionMap.jsx
+++ src/components/ComparisonPoorRichGraphic/GlobalConsumptionMap/GlobalConsumptionMap.jsx
@@ -4,10 +4,9 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 function GlobalConsumptionMap() {
+    
     const mapRef = useRef();
-    const mapContainerRef = useRef();
-    const [hoveredCountry, setHoveredCountry] = useState(null);  // Ensure useState is imported
-    const [meatConsumption, setMeatConsumption] = useState(null);  // Ensure useState is imported
+    const mapContainerRef = useRef(); 
 
 
     useEffect(() => {
@@ -17,7 +16,7 @@ function GlobalConsumptionMap() {
         mapRef.current = new mapboxgl.Map({
             container: mapContainerRef.current,
             center: [11.424338, 36.439782],
-            style: 'mapbox://styles/benmarbe/cm8cyh0u0003301r0506uei0g', // Map style URL
+            style: 'mapbox://styles/benmarbe/cm8cyh0u0003301r0506uei0g', 
             zoom: 1,
             projection: 'mercator',
             maxZoom: 6,
@@ -28,6 +27,9 @@ function GlobalConsumptionMap() {
         const navControl = new mapboxgl.NavigationControl();
         mapRef.current.addControl(navControl, 'top-right');
 
+        // cursor
+        mapRef.current.getCanvas().style.cursor = 'default';
+
         // Handle map resizing
         const handleResize = () => {
             mapRef.current.resize();
@@ -37,37 +39,31 @@ function GlobalConsumptionMap() {
 
         // Create a new popup instance
         const popup = new mapboxgl.Popup({
-            closeButton: false,  // Prevent the popup from having a close button
-            closeOnClick: false  // Prevent the popup from closing when clicking on the map
+            closeButton: false,  
+            closeOnClick: false  
         });
-
-        // Mouse move event to show the popup on hover
         mapRef.current.on('mousemove', (e) => {
-            // Query the features under the mouse cursor
             const features = mapRef.current.queryRenderedFeatures(e.point, {
-                layers: ['consumption-capita-global-9o7zj4']  // Ensure this is the correct layer
+                layers: ['consumption-capita-global-9o7zj4']  
             });
 
             if (features.length > 0) {
-                const feature = features[0];  // Get the first feature under the mouse
-                const countryName = feature.properties.ADMIN || 'No data available';  // Use appropriate property name
-                setHoveredCountry(countryName);  // Update the hovered country in the state
-                const meatConsumption = feature.properties["Kilograms of Meat consumed per capita/year"].toFixed(0) || 'No data available';  // Use appropriate property name
-                setMeatConsumption(meatConsumption);  // Update the hovered country in the state
+                const feature = features[0];  
+                const countryName = feature.properties.ADMIN || 'No data available'; 
+                const meatConsumption = feature.properties["Kilograms of Meat consumed per capita/year"].toFixed(0) || 'No data available';  
 
                 // Set the popup content and position
                 popup.setLngLat(e.lngLat)
-                    .setHTML(`<div id="popup-text"><p id="popup-country">${countryName}:</p><p id="popup-consumption">${meatConsumption} kg of meat consumed per capita in 2021.</p></div>`)
+                    .setHTML(`<div id="popup-text"><p id="popup-country">${countryName}:</p><p id="popup-consumption">${meatConsumption} kg of meat consumed per capita (2021).</p></div>`)
                     .addTo(mapRef.current);
             } else {
-                popup.remove();  // Remove the popup if no features are under the mouse
-                console.log('No features found');
+                popup.remove();  
             }
         });
 
         // Mouse leave event to remove the popup
         mapRef.current.on('mouseleave', () => {
-            popup.remove();  // Remove the popup when the mouse leaves the map
+            popup.remove();  
         });
 
         return () => {
