892f99fe1489f4e340a7ebe58fb2b18ff099aa13
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-06 17:24:31 +0200

    Update MeatIconWrapper.jsx

--- src/components/ComparisonPoorRichGraphic/MeatIconWrapper.jsx
+++ src/components/ComparisonPoorRichGraphic/MeatIconWrapper.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import MeatIconContainer from './MeatIconContainer/MeatIconContainer';
 import useInfoBoxAnimation from '../../hooks/useInfoBoxAnimation';
