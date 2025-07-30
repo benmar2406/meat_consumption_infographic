3cc56db971d0da309423f72b83004a07ad17a0bd
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-30 13:05:32 +0200

    bc

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,4 @@
 import { useRef } from 'react';
-import { useContext } from 'react';
-import { DeviceContext } from "../../context/deviceContext";
 import { useInView } from "framer-motion";
 import { useTranslation } from 'react-i18next';
 
@@ -9,7 +7,6 @@ import './Intro.css'
 function Intro() {
 
   const { t } = useTranslation();
-  const { mobile } = useContext(DeviceContext)
   const ref = useRef(null);
   const isInView = useInView(ref, {once: true});
 
@@ -19,7 +16,7 @@ function Intro() {
           <div className='intro-container'>
             <article className='intro' 
               ref={ref} 
-              style={{opacity: isInView || mobile ? 1 : 0,
+              style={{opacity: isInView ? 1 : 0,
                 transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"}}
               >
                 <h1>{t('intro.title')}</h1>
