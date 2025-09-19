67ccc98dfd30f6158e263a58b7884eac92eccb06
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-09-19 22:04:38 +0200

    Update Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -25,7 +25,7 @@ const Main = ()  => {
     return(
     <>
         
-        {/*<Navigation />*/}
+        <Navigation />  
         <LanguageToggle/>
         <MeatProducedGraphic />
         <Intro />
